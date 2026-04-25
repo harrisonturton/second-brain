13d7ca6deb15a5a5c70c7887c4f7a343c9653989
Harrison Turton <u6386433@anu.edu.au>
2026-04-25 13:24:09 +1000

    swap send icon to provided arrow path

--- web/src/icons/ArrowUpIcon.tsx
+++ web/src/icons/ArrowUpIcon.tsx
@@ -6,14 +6,14 @@ export function ArrowUpIcon(props: SVGProps<SVGSVGElement>) {
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
-      strokeWidth={2}
+      strokeWidth={1.5}
       stroke="currentColor"
       {...props}
     >
       <path
         strokeLinecap="round"
         strokeLinejoin="round"
-        d="M12 19.5V4.5m0 0L5.25 11.25M12 4.5l6.75 6.75"
+        d="M8.25 6.75 12 3m0 0 3.75 3.75M12 3v18"
       />
     </svg>
   )
