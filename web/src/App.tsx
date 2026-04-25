0a8ec797caac4275a345f79626d392802c8e3da0
Harrison Turton <u6386433@anu.edu.au>
2026-04-25 13:06:39 +1000

    add styled-components

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,26 @@
+import styled from 'styled-components'
+
+const Page = styled.main`
+  min-height: 100svh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 16px;
+  padding: 24px;
+`
+
+const Title = styled.h1`
+  margin: 0;
+  font-size: 32px;
+  font-weight: 500;
+`
+
 function App() {
   return (
-    <main>
-      <h1>second brain</h1>
-    </main>
+    <Page>
+      <Title>second brain</Title>
+    </Page>
   )
 }
 
