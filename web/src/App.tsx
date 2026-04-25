b6a006268de31dc6532c3738daef5e2117b5be69
Harrison Turton <u6386433@anu.edu.au>
2026-04-25 13:22:52 +1000

    scaffold chat frame next to sidebar

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,7 @@
 import { observer } from 'mobx-react-lite'
-import styled from 'styled-components'
+import { ChatFrame } from './components/ChatFrame'
 import { Sidebar } from './components/Sidebar'
 import { useRootStore } from './stores/RootStore'
-
-const Main = styled.main`
-  margin-left: 252px;
-  padding: 24px;
-  min-height: 100svh;
-`
 
 const App = observer(function App() {
   useRootStore()
@@ -15,9 +9,7 @@ const App = observer(function App() {
   return (
     <>
       <Sidebar />
-      <Main>
-        <h1>second brain</h1>
-      </Main>
+      <ChatFrame />
     </>
   )
 })
---
+++ web/src/components/ChatFrame.tsx
@@ -0,0 +1,34 @@
+import styled from 'styled-components'
+
+const Frame = styled.div`
+  position: fixed;
+  top: 0;
+  left: 252px;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  flex-direction: column;
+`
+
+const Messages = styled.div`
+  flex: 1;
+  overflow-y: auto;
+  display: flex;
+  justify-content: center;
+`
+
+const Column = styled.div`
+  width: 100%;
+  max-width: 720px;
+  padding: 32px 24px 24px;
+`
+
+export function ChatFrame() {
+  return (
+    <Frame>
+      <Messages>
+        <Column />
+      </Messages>
+    </Frame>
+  )
+}
