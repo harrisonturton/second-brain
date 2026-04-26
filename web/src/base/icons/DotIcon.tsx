d9c5e3152cb0285ebdd8c6256239a11e3f62983f
Harrison Turton <u6386433@anu.edu.au>
2026-04-26 10:18:16 +1000

    switch dot icon to outline

--- web/src/base/icons/DotIcon.tsx
+++ web/src/base/icons/DotIcon.tsx
@@ -5,10 +5,12 @@ export function DotIcon(props: SVGProps<SVGSVGElement>) {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
-      fill="currentColor"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={1.5}
       {...props}
     >
-      <circle cx="12" cy="12" r="3" />
+      <circle cx="12" cy="12" r="3.5" />
     </svg>
   )
 }
